94f663b9eb46c82cfc53859157f7ec180f055dac
naufal <naufal.said19@gmail.com>
2016-10-20 22:57:15 +0700

    oooo

--- DataModel/Model/AbsenceModel.Designer.cs
+++ DataModel/Model/AbsenceModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'D:\HRIS-R03\DataModel\Model\AbsenceModel.edmx'. 
+﻿// T4 code generation is enabled for model 'D:\KERJA\To Nouval\Project\DataModel\Model\AbsenceModel.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
--- DataModel/Model/WebApiDataModel.Designer.cs
+++ DataModel/Model/WebApiDataModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'D:\HRIS-R03\DataModel\Model\WebApiDataModel.edmx'. 
+﻿// T4 code generation is enabled for model 'D:\KERJA\To Nouval\Project\DataModel\Model\WebApiDataModel.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
